4e81ce176e82a1df3eafa50dba61f31b1ae98e44
Jamie Newbon <Jamie@symless.com>
2019-05-02 12:30:33 +0100

    GC, AWS Test push

--- CI/Windows/build.bat
+++ CI/Windows/build.bat
@@ -1,6 +1,4 @@
-SET VS_INSTALL_PATH=C:\"Program Files (x86)"\"Microsoft Visual Studio"\2019\Community\
-call %VS_INSTALL_PATH%Common7\Tools\VsDevCmd.bat
 mkdir build
 cd build
-cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Debug ..
-msbuild synergy-core.sln /p:Platform="x64" /p:Configuration=Debug /m
+cmake -DCMAKE_BUILD_TYPE=Release ..
+msbuild synergy-core.sln /p:Platform="x64" /p:Configuration=Release /m
